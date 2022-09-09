2215ea5bee3a4d86e23b7164fc871d28678ac33e
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-09-09 02:05:37 -0300

    creo archivo actionTypes

---
+++ client/src/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const GET_ALL_POKEMONS = "GET_ALL_POKEMONS";
+export const FILTER_BY_TYPE = "FILTER_BY_TYPE";
+export const FILTER_BY_ORIGIN = "FILTER_BY_ORIGIN";
+export const SORT_BY_ALPHABET = "SORT_BY_ALPHABET";
+export const SORT_BY_ATTACK = "SORT_BY_ATTACK"
+export const CREATE_POKEMON = "CREATE_POKEMON";
+export const GET_POKEMON = "GET_POKEMON";
