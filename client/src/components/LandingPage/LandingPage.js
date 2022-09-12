ad9889bb4d13c2641bef1bd02a31fec1a122d429
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-09-12 01:48:11 -0300

    Landing Page con animación

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,31 +1,34 @@
 import React from 'react';
-import { connect } from 'react-redux';
+//import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
 import './LandingPage.css';
 
+
 export default function LandingPage(props) {
+
 return(
-	<>
-	  
+	
+	  <div className="containerLanding"> 
 		<div id="pop1">
-			<p>{'You can see all first 40, first generation Pokemons!'}</p>
+			<p>{'Wanna remember some of the first generation pokemons?'}</p>
 		</div>
 		<div id="pop2">
-			<p>{'You can search through those Pokemons by name'}</p>
+			<p>{`You'll be able to search them by name`}</p>
 		</div>
 		<div id="pop3">
-			<p>{'You can order them by name or attack'}</p>
+			<p>{'Sort them by attack or alphabetically'}</p>
 		</div>
 		<div id="pop4">
-			<p>{'You can filter them by type'}</p>
+			<p>{'Filter them by type'}</p>
 		</div>
 		<div id="pop5">
-			<p>{'And you can even create your own Pokemon!!'}</p>
+			<p>{'You can even create your own Pokemon!!'}</p>
 		</div>
 		<Link to="/home" id="button">
-			<button>Start</button>
+			<button id="start">Start</button>
 		</Link>
-	 
-	</>
+	  </div>
+	
 	)
 }
