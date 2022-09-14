2d76d4ce09e2416d51cfc317b005622cf34daa6d
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-09-14 12:58:46 -0300

    le doy la url base por default a axios para el deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter } from 'react-router-dom';
+import dotenv from 'dotenv';
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 ReactDOM.render(
   <Provider store= {store}>
